edfe2b751f04043acc2db80daa56bd31d99fa83f
hwen <hwenleung@gmail.com>
2016-02-24 00:53:54 +0800

    .

--- public/src/dist/app.js
+++ public/src/dist/app.js
@@ -261,6 +261,34 @@
 })(angular);
 
 
+(function(angular) {
+    'use strict';
+
+    angular.module('waka').controller('userPageController', ['$scope', '$state', 'User',
+        userPageController]);
+
+    function userPageController($scope, $state, User) {
+        var vm = this;
+        vm.itemList = [{
+            title: "我的提问",
+            url: "question"
+        }, {
+            title: "我的回答",
+            url: "answer"
+        }, {
+            title: "我的收藏",
+            url: "collection"
+        }, {
+            title: "关注话题",
+            url: "topic"
+        }];
+
+        vm.userSetting = function() {
+            $state.go('user-setting');
+        };
+
+    }
+})(angular);
 /**
  * Created by hwen on 16/1/27.
  */
@@ -390,34 +418,6 @@
     }
 })(angular);
 
-(function(angular) {
-    'use strict';
-
-    angular.module('waka').controller('userPageController', ['$scope', '$state', 'User',
-        userPageController]);
-
-    function userPageController($scope, $state, User) {
-        var vm = this;
-        vm.itemList = [{
-            title: "我的提问",
-            url: "question"
-        }, {
-            title: "我的回答",
-            url: "answer"
-        }, {
-            title: "我的收藏",
-            url: "collection"
-        }, {
-            title: "关注话题",
-            url: "topic"
-        }];
-
-        vm.userSetting = function() {
-            $state.go('user-setting');
-        };
-
-    }
-})(angular);
 /**
  * Created by hwen.
  */
